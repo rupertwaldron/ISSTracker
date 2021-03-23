342dedecbe82beb3637de165f75e0b8f567abd84
rupertwaldron <rupert.waldron@yahoo.co.uk>
2021-03-23 17:03:18 +0000

    needs some more work

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 import Marker from "./Marker";
 
 const containerStyle = {
-    width: '1800px',
+    width: '1600px',
     height: '800px'
 };
 
 const center = {
-    lat: 10,
-    lng: 92.9
+    lat: 51.4769,
+    lng: 0.0005
 };
 
 function MyComponent() {
+
+    const [issLocation, setIssLocation] = useState(
+        {
+            issLocation: {
+                issLatitude: 0,
+                issLongitude: 0
+            }
+        })
+
     const { isLoaded } = useJsApiLoader({
         id: 'google-map-script',
         googleMapsApiKey: 'AIzaSyBFx6XeW-AJcXeNBOYYi-NJerP2hv5tisk'
@@ -34,16 +43,16 @@ function MyComponent() {
         <GoogleMap
             mapContainerStyle={containerStyle}
             center={center}
-            zoom={5}
+            zoom={2}
             onLoad={onLoad}
             onUnmount={onUnmount}
         >
             { /* Child components, such as markers, info windows, etc. */ }
             <Marker
-                lat={center.lat}
-                lng={center.lng}
+                // lat={-100}
+                // lng={-100}
                 name="My Marker"
-                color="blue"
+                color="green"
             />
         </GoogleMap>
     ) : <></>
