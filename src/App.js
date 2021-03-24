5a421ec766a2e674245dbec0d2c2b062f9b1f45e
rupertwaldron <rupert.waldron@yahoo.co.uk>
2021-03-24 20:24:21 +0000

    not working that well

--- src/App.js
+++ src/App.js
@@ -7,35 +7,35 @@ const MapContainer = () => {
         {
             name: "Location 1",
             location: {
-                lat: 41.3954,
+                lat: 11.3954,
                 lng: 2.162
             },
         },
         {
             name: "Location 2",
             location: {
-                lat: 41.3917,
+                lat: 31.3917,
                 lng: 2.1649
             },
         },
         {
             name: "Location 3",
             location: {
-                lat: 41.3773,
+                lat: 51.3773,
                 lng: 2.1585
             },
         },
         {
             name: "Location 4",
             location: {
-                lat: 41.3797,
+                lat: 61.3797,
                 lng: 2.1682
             },
         },
         {
             name: "Location 5",
             location: {
-                lat: 41.4055,
+                lat: 81.4055,
                 lng: 2.1915
             },
         }
