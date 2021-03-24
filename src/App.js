62f17ded79a964240fab0007d446ed60f62b8880
rupertwaldron <rupert.waldron@yahoo.co.uk>
2021-03-24 19:32:27 +0000

    markers just about working

--- src/App.js
+++ src/App.js
@@ -8,21 +8,21 @@ const MapContainer = () => {
         {
             name: "Location 1",
             location: {
-                lat: 41.3954,
+                lat: 10.3954,
                 lng: 2.162
             },
         },
         {
             name: "Location 2",
             location: {
-                lat: 41.3917,
+                lat: 20.3917,
                 lng: 2.1649
             },
         },
         {
             name: "Location 3",
             location: {
-                lat: 41.3773,
+                lat: 30.3773,
                 lng: 2.1585
             },
         },
@@ -36,7 +36,7 @@ const MapContainer = () => {
         {
             name: "Location 5",
             location: {
-                lat: 41.4055,
+                lat: 50.4055,
                 lng: 2.1915
             },
         }
